7b043e188656c626982886b0e7754538ad3d6ea0
Florian Schmidt <florian.schmidt.welzow@t-online.de>
2017-04-22 19:14:48 +0200

    Code-Cleanup root directory modules
    The following changes was made to modules and scripts in the root
    directory of this project:
    
    * Changed big if/else control structure for error management to if + early
      return This makes the code a bit easier to read as the indention level is
      kept as low as possible
    * removed commented code
    * fixed some indention levels (not tab/whitespaces)
    * removed unused imports/requires and unused declared variables
    * for a consistent use: changed all " to '
    * removed duplicate varibale declaration
    * merged multiple var definition blocks
    * moved variable declarations to the top of modules/functions
    * Changed usage of type-converting comparsion (equality, ==) to strict comparsion (strict
      equality, ===) where usage of type-converting comparsion doesn't seem to be explicitly
      needed. The same applies for non-equality comparsions (!= -> !==)

--- mailchimp-myohversion.js
+++ mailchimp-myohversion.js
@@ -1,28 +1,35 @@
-var mongoose = require('mongoose')
-    , logger = require('./logger.js');
-var User = require('./models/user');
-var Openhab = require('./models/openhab');
-var config = require('./config.json');
+var mongoose = require('mongoose'),
+    logger = require('./logger.js'),
+    User = require('./models/user'),
+    config = require('./config.json'),
+    Openhab = require('./models/openhab'),
+    mongoConnectionString;
 
-mongoose.connect('mongodb://' + config.mongodb.user + ':' + config.mongodb.password + '@' + config.mongodb.hosts[0] + '/openhab', function(err) {
+mongoConnectionString = 'mongodb://' + config.mongodb.user +
+    ':' + config.mongodb.password +
+    '@' + config.mongodb.hosts[0] +
+    '/openhab';
+
+mongoose.connect(mongoConnectionString, function (err) {
     if (err) {
-        logger.error("openHAB-cloud: mongo connection error: " + err);
-    } else {
-        logger.info("openHAB-cloud: connected to mongodb");
-        Openhab.find({$or:[{clientVersion:'1.4.0.1'}, {clientVersion:'1.4.0.2'}]}, function(error, openhabs) {
-            User.find({role:'master'}, function(error, users) {
-                var usersArray = {};
-                for (var j=0; j<users.length; j++) {
-                    usersArray[users[j].account] = users[j];
-                }
-                // console.log(usersArray);
-                for (var i=0; i<openhabs.length; i++) {
-                    if (usersArray[openhabs[i].account] != null) {
-                        console.log(usersArray[openhabs[i].account].username);
-                    }
-                }
-                process.exit(code=0);
-            });
-        });
+        logger.error('openHAB-cloud: mongo connection error: ' + err);
+        return;
     }
+
+    logger.info('openHAB-cloud: connected to mongodb');
+    Openhab.find({$or:[{clientVersion:'1.4.0.1'}, {clientVersion:'1.4.0.2'}]}, function (error, openhabs) {
+        User.find({role:'master'}, function(error, users) {
+            var usersArray = {};
+            for (var j=0; j<users.length; j++) {
+                usersArray[users[j].account] = users[j];
+            }
+
+            for (var i=0; i<openhabs.length; i++) {
+                if (usersArray[openhabs[i].account] !== null) {
+                    console.log(usersArray[openhabs[i].account].username);
+                }
+            }
+            process.exit(code=0);
+        });
+    });
 });
--- makeinvites.js
+++ makeinvites.js
@@ -1,33 +1,35 @@
-var config = require('./config.json');
+var config = require('./config.json'),
+    mongoose = require('mongoose'),
+    uuid = require('uuid'),
+    mongooseTypes = require('mongoose-types'),
+    Invitation = require('./models/invitation'),
+    mongoConnectionString;
 
 // MongoDB connection
-var mongoose = require('mongoose');
-var uuid = require('uuid');
-mongoose.connect('mongodb://' + config.mongodb.user + ':' + config.mongodb.password + '@' + config.mongodb.hosts[0] + '/openhab');
-var mongooseTypes = require("mongoose-types");
+mongoConnectionString = 'mongodb://' + config.mongodb.user +
+    ':' + config.mongodb.password +
+    '@' + config.mongodb.hosts[0] +
+    '/openhab';
+
+mongoose.connect(mongoConnectionString);
 mongooseTypes.loadTypes(mongoose);
 
-// Mongoose models
-var User = require('./models/user');
-var Openhab = require('./models/openhab');
-var Event = require('./models/event');
-var Invitation = require('./models/invitation');
-
-console.log("Making 10 invitations!");
+console.log('Making 10 invitations!');
 for (i=0; i<10; i++) {
+    var invitationCode,
+        invite;
+
     invitationCode = uuid.v1();
-    invite = new Invitation({code: invitationCode, email: "openhab@openhab.org"});
-    console.log("openHAB-cloud: New invitation code " + invitationCode);
-    invite.save(function(err, invitation) {
-        console.log("callback");
+    invite = new Invitation({code: invitationCode, email: 'openhab@openhab.org'});
+    console.log('openHAB-cloud: New invitation code ' + invitationCode);
+    invite.save(function (err, invitation) {
+        console.log('callback');
         if (err) {
-            console.log("Error");
+            console.log('Error');
         } else {
-            console.log("Saved: " + invitation);
+            console.log('Saved: ' + invitation);
         }
     });
 }
 
-//mongoose.disconnect();
-console.log("Complete!");
-//process.exit();
+console.log('Complete!');
