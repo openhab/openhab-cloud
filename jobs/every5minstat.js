2a513e02b99f1efd92e4fdc09388f241a443d770
Florian <florian.schmidt.welzow@t-online.de>
2017-05-01 16:27:48 +0200

    Cleanup jobs modules (#85)
    The following changes was made to scripts in the jobs directory of this project:
    
    * Changed big if/else control structure for error management to if + early
      return This makes the code a bit easier to read as the indention level is
      kept as low as possible
    * removed commented code
    * removed unused imports/requires and unused declared variables
    * for a consistent use: changed all " to '
    * merged multiple var definition blocks
    * moved variable declarations to the top of modules/functions
    * Changed usage of type-converting comparsion (equality, ==) to strict comparsion (strict
      equality, ===) where usage of type-converting comparsion doesn't seem to be explicitly
      needed. The same applies for non-equality comparsions (!= -> !==)
    
    In this commit, the every5minstat.js script is rewritten to fulfill the things above and
    make the code a lot smaller.

--- jobs/every5minstat.js
+++ jobs/every5minstat.js
@@ -1,55 +1,90 @@
-var cronJob = require('cron').CronJob
-    , logger = require('../logger')
-    , redis = require('../redis-helper');
-// Mongoose models
-var User = require('../models/user');
-var Openhab = require('../models/openhab');
-var OpenhabConfig = require('../models/openhabconfig');
-var Event = require('../models/event');
-var Item = require('../models/item');
-var UserDevice = require('../models/userdevice');
-var UserAccount = require('../models/useraccount');
-var Notification = require('../models/notification');
-var AccessLog = require('../models/accesslog');
-var OpenhabAccessLog = require('../models/openhabaccesslog');
-var Invitation = require('../models/invitation');
-var Myohstat = require('../models/myohstat');
+var cronJob = require('cron').CronJob,
+    logger = require('../logger'),
+    redis = require('../redis-helper'),
+    // Mongoose models
+    User = require('../models/user'),
+    Openhab = require('../models/openhab'),
+    UserDevice = require('../models/userdevice'),
+    Invitation = require('../models/invitation'),
+    Myohstat = require('../models/myohstat'),
+    stats = [],
+    openhabCount, userCount, openhabOnlineCount, invitationUsedCount,
+    invitationUnusedCount, userDeviceCount;
 
-module.exports = new cronJob('00 */5 * * * *', function() {
-    logger.info("openHAB-cloud: every5min statistics collection job started");
-    Openhab.count({}, function(err, openhabCount) {
-        if (!err) {
-            Openhab.count({status: 'online'}, function(err, openhabOnlineCount) {
-                if (!err) {
-                    User.count({}, function(err, userCount) {
-                        if (!err) {
-                            Invitation.count({used:true}, function(err, invitationUsedCount) {
-                                if (!err) {
-                                    Invitation.count({used:false}, function(err, invitationUnusedCount) {
-                                        if (!err) {
-                                            UserDevice.count({}, function(err, userDeviceCount) {
-                                                if (!err) {
-                                                    var newStat = new Myohstat({uC: userCount, oC: openhabCount,
-                                                        ooC: openhabOnlineCount, iuC: invitationUsedCount,
-                                                        iuuC: invitationUnusedCount, udC: userDeviceCount});
-                                                    newStat.save();
-                                                    // Set current statistics to redis
-                                                    redis.mset(["openhabCount", openhabCount, "openhabOnlineCount", openhabOnlineCount,
-                                                        "userCount", userCount, "invitationUsedCount", invitationUsedCount,
-                                                        "invitationUnusedCount", invitationUnusedCount,
-                                                        "userDeviceCount", userDeviceCount, "last5MinStatTimestamp", new Date()], function(err, result) {
-                                                        logger.info("openHAB-cloud: every5min statistics collection job finished");
-                                                    });
-                                                }
-                                            });
-                                        }
-                                    });
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }
+/**
+ * Callback function for a count operation on a Mongoose model. It will save the count in the
+ * stats object, if there was no error, and calls the saveStats function to save the
+ * statistics, if all counts are finished.
+ *
+ * Tis function expects, that the this argument is the name of the statistic value.
+ *
+ * @private
+ */
+function countCallback (err, count) {
+    if (!err) {
+        stats[this] = count;
+    }
+}
+
+/**
+ * Checks, if all callbacks are executed and if so, validates the stats object. It will then, if the
+ * validation succeeds, save the values to redis.
+ */
+function saveStats() {
+    var newStat;
+
+    // validate the results
+    if (Object.keys(stats).length !== 6) {
+        // not all data could be retrieved
+        logger.info('The length of the stats object does not match the expected one, can not save statistical data.');
+        return;
+    }
+
+    newStat = new Myohstat({
+        uC: stats['userCount'],
+        oC: stats['openhabCount'],
+        ooC: stats['openhabOnlineCount'],
+        iuC: stats['invitationUsedCount'],
+        iuuC: stats['invitationUnusedCount'],
+        udC: stats['userDeviceCount']
     });
+    newStat.save();
+
+    // Set current statistics to redis
+    redis.mset(
+        [
+            'openhabCount',
+            stats['openhabCount'],
+            'openhabOnlineCount',
+            stats['openhabOnlineCount'],
+            'userCount',
+            stats['userCount'],
+            'invitationUsedCount',
+            stats['invitationUsedCount'],
+            'invitationUnusedCount',
+            stats['invitationUnusedCount'],
+            'userDeviceCount',
+            stats['userDeviceCount'],
+            'last5MinStatTimestamp',
+            new Date()
+        ],
+        function (err, result) {
+            logger.info('openHAB-cloud: every5min statistics collection job finished');
+        }
+    );
+}
+
+module.exports = new cronJob('00 */5 * * * *', function () {
+    var promises = [];
+
+    logger.info('openHAB-cloud: every5min statistics collection job started');
+
+    promises.push(Openhab.count({}, countCallback.bind('openhabCount')).exec());
+    promises.push(Openhab.count({status: 'online'}, countCallback.bind('openhabOnlineCount')).exec());
+    promises.push(User.count({}, countCallback.bind('userCount')).exec());
+    promises.push(Invitation.count({used:true}, countCallback.bind('invitationUsedCount')).exec());
+    promises.push(Invitation.count({used:false}, countCallback.bind('invitationUnusedCount')).exec());
+    promises.push(UserDevice.count({}, countCallback.bind('userDeviceCount')).exec());
+
+    Promise.all(promises).then(saveStats);
 });
