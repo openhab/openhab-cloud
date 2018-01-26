861d8f154c671dd10cfcd7cabe7068c9325b7b1d
Mehmet Arziman <mehmet@arziman.de>
2018-01-26 02:25:15 +0100

    Fix for email sending and missing verification mail (#156)
    * Follow up PR 120 (#121)
    
    * Follow up PR 120
    
     * Consistently use let/const or var in one file
     * Let the try/catch block do it's work when a configuration
       variable does not exist
    
    * Fix code style issues
    
    * Added .project file to ignore list
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Fixed issue w. bcrypt & raised node version to 7.10.1(security-patch)
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Corrected year in footer to 2018
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Re-correct config files
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * New OH Logo added
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Small logo size adjustment
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Small OHC version increase, module version adjustments
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Docu updates and hints
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Added new logo (favicon.ico)
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Fixes case sensitive bug in Login/Registration
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Fix for making the openhabsecret input field to password field
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Minor password fix and trim() function
    - Fixes a bug that shows the pw in plain text while registration error
    - Brings the trim() method back for storing the username
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>
    
    * Fix for email sending and registration verification mail
    
    Signed-off-by: Mehmet Arziman <mehmet@arziman.de>

--- mailer.js
+++ mailer.js
@@ -1,54 +1,71 @@
-// This is an utility class to send e-mails to a user on behalf of openhab-cloud
+//This is an utility class to send e-mails to a user on behalf of openhab-cloud
 var path = require('path'),
-    templatesDir   = path.resolve(__dirname, '.', 'templates'),
-    emailTemplates = require('email-templates'),
-    nodemailer     = require('nodemailer'),
-    logger = require('./logger.js'),
-    app = require('./app'),
-    productionEnv = process.env.NODE_ENV || 'dev';
+templatesDir   = path.resolve(__dirname, '.', 'templates'),
+nodemailer     = require('nodemailer'),
+logger = require('./logger.js'),
+app = require('./app'),
+productionEnv = process.env.NODE_ENV || 'dev';
+
+const Email = require('email-templates');
 
 if (productionEnv === 'production') {
-    module.exports.sendEmail = function (email, subject, templateName, locals, cb) {
-        emailTemplates(templatesDir, function (err, template) {
-            if (err) {
-                cb(err);
-            } else {
-                var transport = nodemailer.createTransport('SMTP', {
-                    host: app.config.mailer.host,
-                    port: app.config.mailer.port,
-                    secureConnection: app.config.mailer.secureConnection,
-                    auth: {
-                        user: app.config.mailer.user,
-                        pass: app.config.mailer.password
-                    }
-                });
-                template(templateName, locals, function (err, html, text) {
-                    if (err) {
-                        cb(err);
-                    } else {
-                        transport.sendMail( {
-                            from: app.config.mailer.from,
-                            to: email,
-                            subject: subject,
-                            html: html,
-                            generateTextFromHTML: true
-                        }, function(err, responseStatus) {
-                            if (err) {
-                                cb(err)
-                            } else {
-                                cb (null);
-                            }
-                        });
-                    }
-                });
-            }
-        });
-    }
-} else {
-    logger.info('openHAB-cloud: Mailer will emulate sending in development environment');
-    module.exports.sendEmail = function (email, subject, templateName, locals, cb) {
-        logger.info('openHAB-cloud: Emulating sendEmail to ' + email + ' about ' + subject);
-        cb(null);
-    }
-}
+	module.exports.sendEmail = function(email, subject, templateName, locals,
+			cb) {
+		try {
+			var smtpConfig = {
+					host : app.config.mailer.host,
+					port : app.config.mailer.port,
+					secure : app.config.mailer.secureConnection, // use SSL
+					tls : {
+						rejectUnauthorized : false
+					},
+					auth : {
+						user : app.config.mailer.user,
+						pass : app.config.mailer.password
+					}
+			};
+			var transport = nodemailer.createTransport(smtpConfig);
+		} catch (error) {
+			logger
+			.error('openHAB-cloud: sendMail error occured during SMTP transport: '
+					+ error);
+		}
+		try {
+			var message = {
+					from : app.config.mailer.from,
+					to : email,
+					subject : subject,
+					generateTextFromHTML : true
+			};
+			emailsender = new Email({
+				views : {
+					root : templatesDir,
+					options : {
+						extension : 'ejs'
+					}
+				},
+				transport : transport
+			});
 
+			emailsender.send({
+				template : templateName,
+				message : message,
+				locals : locals
+			});
+			cb(null);
+		} catch (error) {
+			logger
+			.error('openHAB-cloud: sendMail error occured during sending: '
+					+ error);
+		}
+	}
+} else {
+	logger
+	.info('openHAB-cloud: Mailer will emulate sending in development environment');
+	module.exports.sendEmail = function(email, subject, templateName, locals,
+			cb) {
+		logger.info('openHAB-cloud: Emulating sendEmail to ' + email
+				+ ' about ' + subject);
+		cb(null);
+	}
+}
