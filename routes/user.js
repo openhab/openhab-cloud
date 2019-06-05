8a8b817874c75492363c4b51b1916a2fe0c7c4d8
RayBa82 <therayba@gmail.com>
2019-06-05 11:42:05 +0200

    Added API to get the current user id, as needed for Google Assistant
    Signed-off-by: RayBa82 <therayba@gmail.com>

--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,8 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.useridget = function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ userId: req.user.id }));
+};
