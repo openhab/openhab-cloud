0fc8ccbb96dea3d9fd024cf9a155853f87471b62
Wouter Born <github@maindrain.net>
2019-10-11 11:25:54 +0200

    Update license to EPL-2.0 (#282)
    Updates the LICENSE file some headers and adds a NOTICE file.
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- deployment/openshift/openshift-undeploy.sh
+++ deployment/openshift/openshift-undeploy.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+#
+# Copyright (c) 2010-2019 Contributors to the openHAB project
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+#
 
-###############################################################################
-# Copyright (c) 2018, home-iX and others.
-#
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v1.0
-# which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html
-#
 # Contributors:
 #    Mehmet Arziman (home-iX) - initial contribution
-#
-###############################################################################
 
 set -e
 
