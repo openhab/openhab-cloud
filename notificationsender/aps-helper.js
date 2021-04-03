f30c468cef630d3466c123a7df4338ad39336bca
Dan Cunningham <dan@digitaldan.com>
2021-04-03 14:23:54 -0700

    Disables APNS if no APN or APN signingKey config is present
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- notificationsender/aps-helper.js
+++ notificationsender/aps-helper.js
@@ -1,14 +1,17 @@
 const { APNS, Notification, Errors } = require('apns2'),
     app = require('./../app'),
-    logger = require('./../logger'),
+    logger = require('./../logger');
+let client = null;
+
+if (app.config.apn && app.config.apn.signingKey) {
     client = new APNS({
         ...app.config.apn,
         signingKey: fs.readFileSync(`${app.config.apn.signingKey}`)
     });
-
-client.on(Errors.error, (err) => {
-    logger.error(`openHAB-cloud: APN error ${err.reason} ${err.statusCode} ${err.notification.deviceToken}`)
-})
+    client.on(Errors.error, (err) => {
+        logger.error(`openHAB-cloud: APN error ${err.reason} ${err.statusCode} ${err.notification.deviceToken}`)
+    })
+}
 
 module.exports.sendAppleNotification = function (deviceToken, message, payload) {
     logger.debug(`aps-helper sending ${message} to device ${deviceToken}`)
@@ -20,5 +23,7 @@ module.exports.sendAppleNotification = function (deviceToken, message, payload) 
         },
         ...(payload)
     })
-    client.send(notification).catch(err => { });
+    if (client) {
+        client.send(notification).catch(err => { });
+    }
 }
