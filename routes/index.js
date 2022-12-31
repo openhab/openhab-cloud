55489bf96a322977b1eef24f7043a4c11e17c908
Javaded <j.effatdoost@gmail.com>
2022-12-31 10:28:11 +0330

    fixing req.logout()
    Signed-off-by: Javaded <j.effatdoost@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -73,8 +73,9 @@ Routes.prototype.setupGeneralRoutes = function (app) {
 };
 
 Routes.prototype.setupLoginLogoutRoutes = function (app) {
-    app.get('/logout', function (req, res) {
-        req.logout();
+    app.get('/logout', function (req, res, next) {
+        req.logout(function(err) {
+        if (err) { return next(err); }
         res.redirect('/');
     });
 
