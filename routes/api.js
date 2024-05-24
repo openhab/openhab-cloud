82e96a336cdd26c9ec09884411c42230cc7cc2ee
Dan Cunningham <dan@digitaldan.com>
2024-05-24 12:43:49 -0700

    Use single ticks
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- routes/api.js
+++ routes/api.js
@@ -4,27 +4,27 @@ var logger = require('../logger');
 var system = require('../system');
 var firebase = require('../notificationsender/firebase');
 
-exports.notificationsget = function(req, res) {
+exports.notificationsget = function (req, res) {
     var limit = req.query.limit > 0 ? parseInt(req.query.limit) : 10,
-    skip = req.query.skip > 0 ? parseInt(req.query.skip) : 0;
-    Notification.find({user: req.user.id}, '-user')
+        skip = req.query.skip > 0 ? parseInt(req.query.skip) : 0;
+    Notification.find({ user: req.user.id }, '-user')
         .limit(limit)
         .skip(skip)
-        .sort({created: 'desc'})
-        .exec(function(error, notifications) {
-        if (!error) {
-            res.status(200).json(notifications);
-        } else {
-            return res.status(500).json({
-                errors: [{
-                    message: "Error getting notifications"
-                }]
-            });
-        }
-    });
+        .sort({ created: 'desc' })
+        .exec(function (error, notifications) {
+            if (!error) {
+                res.status(200).json(notifications);
+            } else {
+                return res.status(500).json({
+                    errors: [{
+                        message: "Error getting notifications"
+                    }]
+                });
+            }
+        });
 };
 
-exports.notificationssettingsget = function(req, res) {
+exports.notificationssettingsget = function (req, res) {
     var config = {};
     if (system.isGcmConfigured()) {
         config.gcm = {
@@ -60,8 +60,8 @@ exports.hidenotification = function (req, res) {
     });
 }
 
-exports.proxyurlget = function(req, res) {
+exports.proxyurlget = function (req, res) {
     res.status(200).json({
-        "url": system.getProxyURL()
+        'url': system.getProxyURL()
     });
 };
