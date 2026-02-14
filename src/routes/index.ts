e732dd17b81ba7a201cbcc4d35f222b84f3c30c7
Dan Cunningham <dan@digitaldan.com>
2026-02-14 04:52:44 -0800

    oauth changes
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -604,7 +604,7 @@ export function createRoutes(deps: RoutesDependencies): Router {
   router.get('/oauth2/authorize', ensureAuthenticated, ...oauth2Controller.authorization);
   router.post('/oauth2/authorize/decision', ensureAuthenticated, ...oauth2Controller.decision);
   router.post('/oauth2/token',
-    passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
+    passport.authenticate(['oAuthBasic', 'oauth2-client-password'], { session: false }),
     ...oauth2Controller.token);
 
   // ============================================
